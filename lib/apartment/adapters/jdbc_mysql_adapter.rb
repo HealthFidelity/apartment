91c892538cb0d1ba1bcee7aeee41455fcdfa7f19
Ajinkya Pisal <ajinkya.pisal@healthfidelity.com>
2018-01-17 12:21:22 +0530

    Fixed TenantNotFound error

--- lib/apartment/adapters/jdbc_mysql_adapter.rb
+++ lib/apartment/adapters/jdbc_mysql_adapter.rb
@@ -4,12 +4,20 @@ module Apartment
 
   module Tenant
     def self.jdbc_mysql_adapter(config)
-      Adapters::JDBCMysqlAdapter.new config
+      adapter = Adapters::JDBCMysqlAdapter.new(config)
+      adapter.process_excluded_models
+      adapter
     end
   end
 
   module Adapters
     class JDBCMysqlAdapter < AbstractJDBCAdapter
+
+      def initialize(config)
+        super
+        Apartment.connection_handler = ActiveRecord::ConnectionAdapters::ConnectionHandler.new
+        reset
+      end
 
       def reset_on_connection_exception?
         true
